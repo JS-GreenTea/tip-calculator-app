969ac3256cc8d8d55e9e3bb1362becf3c93db1bc
2yunseong <dbsdltjd3701@naver.com>
2022-11-04 23:05:33 +0900

    feat: 금액 입력이 유효한지 판단하는 기능

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ const billInput = document.querySelector("#bill-input");
 const tipAmountResult = document.querySelector("#tip-amount-result");
 const totalResult = document.querySelector("#total-result");
 let bill = 0;
+
+const renderWarningStatus = () => {
+  // some logic...
+};
+
+const isUnVaildInput = (target) => {
+  return isNaN(target);
+};
+
 const render = () => {
+  if (isUnVaildInput(bill)) {
+    // handle bad input ...
+    console.log("bad bill input!");
+  }
   console.log("bill:", bill);
 };
 
