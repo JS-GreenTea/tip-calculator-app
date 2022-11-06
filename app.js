255fb3f8dd72d894ef0eefe6a9f08ac14000b243
baegyeong <beth0929@naver.com>
2022-11-06 19:27:49 +0900

    feat: 버튼 클릭이벤트 구현

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const totalRatio = document.querySelector(".total-ratio");
 const inputBill = document.querySelector(".bill");
 const selectBtn = document.querySelectorAll("button");
 const peopleNum = document.querySelector(".number");
+const resetBtn = document.querySelector(".reset");
 let selectTip;
 
-tipRatio.innerText = "$0.00";
-totalRatio.innerText = "$0.00";
-
 function clickedBtn(event) {
+  resetBtnColor();
+  event.target.classList.add("clicked");
   selectTip = event.target.innerText;
   selectTip = parseInt(selectTip) / 100;
+}
+
+function resetBtnColor() {
+  for (let i = 0; i < Object.keys(selectBtn).length; i++) {
+    selectBtn[i].classList.remove("clicked");
+  }
 }
 
 for (let i = 0; i < selectBtn.length; i++) {
@@ -23,5 +29,13 @@ function showResult() {
   tipRatio.innerText = `$${tipCalc.toFixed(2)}`;
   totalRatio.innerText = `$${totalCalc.toFixed(2)}`;
 }
+function reset() {
+  inputBill.value = null;
+  peopleNum.value = null;
+  tipRatio.innerText = "$0.00";
+  totalRatio.innerText = "$0.00";
+}
 
 peopleNum.addEventListener("keyup", showResult);
+window.addEventListener("load", reset);
+resetBtn.addEventListener("click", reset);
